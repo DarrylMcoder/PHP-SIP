631d62e4be225d695377cb418ae48fccc220c2d7
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-06-10 09:48:14 -0400

    Add files via upload

---
+++ examples/phpsip.php
@@ -0,0 +1,19 @@
+<?PHP
+    
+require_once('../PhpSIP.class.php');
+
+try {
+  $api = new PhpSIP();/* IP we will bind to*/  
+  $api->setMethod('MESSAGE');  
+  $api->setFrom('sip:darryl@darrylmcoder.onsip.com');
+  $api->setUri('tel:+1-519-589-9829');
+  $api->setBody('Hi, can we meet at 5pm   today?');
+  
+  $res = $api->send(); echo "res1: $res\n";
+
+} catch (Exception $e) {
+
+  echo $e->getMessage()."\n";
+}
+
+?> 
