bff9f347d477011eb2666897b635a3be62f4be71
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-06-11 10:35:59 -0400

    Add files via upload

--- examples/txt.php
+++ examples/txt.php
@@ -35,14 +35,13 @@
       $api->addHeader('Content-Encoding: text/plain');
       $api->addHeader('Content-Disposition: alert');
       $api->addHeader('Content-Language: en');
-     // $api->addHeader('');
-      $api->setBody('\n<h1>'.$msg.'</h1>');
+      $api->addHeader('From: sip:c2c@php-sip.herokuapp.com/examples/txt.php');
+      $api->setBody('<h1>'.$msg.'</h1>');
       $api->setMethod('MESSAGE');
       $api->setFrom('sip:c2c@'.$api->getSrcIp());
       $api->setUri($to);
  
       $res = $api->send();
-      echo "Body".$api->getBody();
  
       if ($res == 200) { 
         $api->listen('MESSAGE');
