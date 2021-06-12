95cf9b46746b18945b7de2650c7276818504bef2
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-06-12 11:48:10 -0400

    Add files via upload

---
+++ examples/register.php
@@ -0,0 +1,14 @@
+<?PHP
+    
+require('../PhpSIP.class.php');
+
+$api = new \PhpSIP();
+$api->setMethod("REGISTER");
+$api->setURI("sip:sip.mcast.net");
+$api->setFrom("sip:c2c@".$api->getSrcIp());
+$api->setTo("sip:darryl@sip.onsip.com");
+$api->setContact("sip:c2c@".$api->getSrcIp());
+$api->addHeader("Expires: 86400");
+$api->send();
+    
+?>
