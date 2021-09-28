16bdaed48a09c86f6fed84f4e00821a8b7d38c50
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-09-28 15:10:34 -0400

    Add files via upload

--- examples/txt.php
+++ examples/txt.php
@@ -1,27 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<?php 
  
-<head><title>PHP-SIP Click to Call</title></head>
- 
-<body>
- 
-<?php if (isset($_POST['to'])) : ?>
- 
-  <?php require_once('..//PhpSIP.class.php') ?>
- 
-  <?php $to = $_POST['to'] ?>
-  <?php $msg = $_POST['msg'] ?>
- 
-  Trying to send  <?php echo $msg ?> to <?php echo $to ?> ...<br />
- 
-  <?php flush() ?>
- 
-  <pre>
-  <?php 
- 
+  require('..//PhpSIP.class.php');
+  require('../myPhpSIP.php');
+  
     try{
  
-      $api = new PhpSIP();
+      $api = new myPhpSIP();
       // if you get "Failed to obtain IP address to bind. Please set bind address manualy."
       // error, use the line below instead
       // $api = new PhpSIP('you_server_IP_address');
@@ -36,19 +20,16 @@
       $api->addHeader('Content-Disposition: alert');
       $api->addHeader('Content-Language: en');
       $api->setFrom('sip:c2c@php-sip.herokuapp.com');
-      $api->setBody('<h1>'.$msg.'</h1>');
+      $api->setBody("Just testing");
       $api->setMethod('MESSAGE');
       //$api->setFrom('sip:c2c@'.$api->getSrcIp());
-      $api->setUri($to);
+      $api->setUri("sip:+15195899829@sip.linphone.org");
  
       $res = $api->send();
  
       if ($res == 200) { 
         $api->listen('MESSAGE');
         $api->reply(200,'OK');
-        $api->setMethod('MESSAGE');
-        $api->addHeader('OK');
-        $api->send();
       }
  
       if ($res == 'No final response in 5 seconds.') {
@@ -63,24 +44,3 @@
       echo "Opps... Caught exception:";
       echo $e;
     }
- 
-  ?>
-  </pre>
-  <hr />
- 
-  <a href="<?php echo $_SERVER['PHP_SELF']; ?>">Back</a>
- 
-<?php else : ?>
- 
-  <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-    <fieldset>
-      To: <input type="text" name="to" size="25" value="sip:enum-test@sip.nemox.net" />
-      <input type="text" name="msg" >
-      <input type="submit" value="Send" />
-    </fieldset>
-  </form>
- 
-<?php endif ?>
- 
-</body>
-</html>
